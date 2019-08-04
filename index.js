797fc9c59533c4c3cb3d898985d37d61c6092166
Nour Alhuda Alasali <nouralasali@hotmail.com>
2019-08-04 00:04:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event){
+  const messageArr = [];
+  for (let i = 0; i < names.length; i++){
+    messageArr[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift.`;
+  }
+  return messageArr;
+}
+  
+function countdown(number){
+  while(number >=0)
+  {
+    console.log(number);
+    number--;
+  }
+} 
